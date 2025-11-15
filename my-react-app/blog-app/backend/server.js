e4446dce754693d970c2019a1c3fcb581c34b3a1
Harshada Kishorilal Rundhey <harshadarundhey@gmail.com>
2025-11-15 16:20:58 +0530

    mini.package

--- my-react-app/blog-app/backend/server.js
+++ my-react-app/blog-app/backend/server.js
@@ -4,14 +4,21 @@ const cors = require('cors');
 const blogRoutes = require('./routes/blogs');
 
 const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/blogApp', {
+// MongoDB connection
+mongoose.connect('mongodb://127.0.0.1:27017/blogApp', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
 
+// Routes
 app.use('/blogs', blogRoutes);
 
-app.listen(5000, () => console.log('Backend running on port 3000'));
+// Start server
+app.listen(5000, () => console.log('Backend running on port 5000'));
